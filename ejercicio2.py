e8956b973456675ceb8c1fc8cd4a18f8e8837e2f
xXquesogamer777Xx <152286555+xXquesogamer777Xx@users.noreply.github.com>
2024-05-22 10:15:45 -0300

    Create ejercicio2.py

---
+++ ejercicio2.py
@@ -0,0 +1,5 @@
+nombre_personaje = input ("ingrese el nombre")
+num_pociones = int (input("ingrese el numero de pociones magicas que posee:"))
+print {nombre_personaje} {num-pociones}
+
+                    
